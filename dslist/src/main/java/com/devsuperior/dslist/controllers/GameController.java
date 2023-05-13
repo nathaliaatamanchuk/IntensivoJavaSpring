723e47a6dbc00d10e61d56345176cff02da4e127
nathaliaatamanchuk <nathaliaatamanchuk5@gmail.com>
2023-05-13 14:01:23 -0300

    Retornando dados interessantes DTO

--- dslist/src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.controllers;
 
 import java.util.List;
 
+import com.devsuperior.dslist.dto.GameMinDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,8 @@ public class GameController {
     private GameService gameService;
 
     @GetMapping
-    public List<Game> findAll(){
-        List<Game> result = gameService.findAll();
+    public List<GameMinDTO> findAll(){
+        List<GameMinDTO> result = gameService.findAll();
         return result;
     }
 
--- dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -15,9 +15,10 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-    public List<Game> findAll() {
+    public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        return result;
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
     }
 
 }
